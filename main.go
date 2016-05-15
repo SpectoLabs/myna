41673080a24b07223038857276fc065a5cec467e
Bart Spaans <bart.spaans@gmail.com>
2016-05-15 04:31:51 +0100

    working capture and playback

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ import (
     "encoding/base64"
 )
 
-type CommandResult struct {
+type Process struct {
     Command []string
     Stdout []byte
     Stderr []byte
     ReturnCode int
 }
 
-type CommandResultJson struct {
+type ProcessJson struct {
     Command []string
     Stdout string
     Stderr string
@@ -29,30 +29,41 @@ type CommandResultJson struct {
 func encodeBase64(str []byte) string {
     return base64.StdEncoding.EncodeToString(str)
 }
+func decodeBase64(str string) []byte {
+    data, _ := base64.StdEncoding.DecodeString(str)
+    return data
+}
 
-func (cmd *CommandResult) Json() []byte {
-    payload := CommandResultJson{}
-    payload.Command = cmd.Command
-    payload.ReturnCode = cmd.ReturnCode
-    payload.Stdout = encodeBase64(cmd.Stdout)
-    payload.Stderr = encodeBase64(cmd.Stderr)
+func (p *Process) Json() []byte {
+    payload := ProcessJson{}
+    payload.Command = p.Command
+    payload.ReturnCode = p.ReturnCode
+    payload.Stdout = encodeBase64(p.Stdout)
+    payload.Stderr = encodeBase64(p.Stderr)
     result, _ := json.Marshal(payload)
-    fmt.Println((string)(result))
     return result
 }
 
-func (cmd *CommandResult) Key() []byte {
-    return ([]byte)(strings.Join(cmd.Command, " "))
+func (p *Process) FromJson(payload []byte) {
+    result := ProcessJson{}
+    json.Unmarshal(payload, &result)
+    p.Stdout = decodeBase64(result.Stdout)
+    p.Stderr = decodeBase64(result.Stderr)
+    p.ReturnCode = result.ReturnCode
 }
 
-func (cmd *CommandResult) Print() {
-    fmt.Println("Command: " + strings.Join(cmd.Command, " "))
-    fmt.Println("Stdout: " + string(cmd.Stdout))
-    fmt.Println("Stderr: " + string(cmd.Stderr))
-    fmt.Printf("Return code: %d\n", cmd.ReturnCode)
+func (p *Process) Key() []byte {
+    return ([]byte)(strings.Join(p.Command, " "))
 }
 
-func Save(cmd *CommandResult) error {
+func (p *Process) Print() {
+    fmt.Println("Command: " + strings.Join(p.Command, " "))
+    fmt.Println("Stdout: " + string(p.Stdout))
+    fmt.Println("Stderr: " + string(p.Stderr))
+    fmt.Printf("Return code: %d\n", p.ReturnCode)
+}
+
+func (p *Process) Save() error {
     db, err := bolt.Open("processes.db", 0600, nil)
     if err != nil {
         return err
@@ -63,38 +74,66 @@ func Save(cmd *CommandResult) error {
         if err != nil {
             return fmt.Errorf("create bucket: %s", err)
         }
-        return b.Put(cmd.Key(), cmd.Json())
+        return b.Put(p.Key(), p.Json())
     })
-    if err != nil {
-        return err
-    }
-    return nil
+    return err
 }
 
-func main() {
+func (p *Process) Capture() {
     cmd := exec.Command(os.Args[1], os.Args[2:]...)
     var stdout bytes.Buffer
     var stderr bytes.Buffer
-    result := CommandResult{}
-    result.Command = os.Args[1:]
     cmd.Stdout = &stdout
     cmd.Stderr = &stderr
-    err := cmd.Run()
-    if err != nil {
+    if err := cmd.Run() ; err != nil {
         if exitError, ok := err.(*exec.ExitError); ok {
             waitStatus := exitError.Sys().(syscall.WaitStatus)
-            result.ReturnCode = waitStatus.ExitStatus()
+            p.ReturnCode = waitStatus.ExitStatus()
         } else {
             fmt.Println(err.Error())
             return
         }
     }
-    result.Stdout = stdout.Bytes()
-    result.Stderr = stderr.Bytes()
-    result.Print()
-    err = Save(&result)
+    p.Stdout = stdout.Bytes()
+    p.Stderr = stderr.Bytes()
+    err := p.Save()
     if err != nil {
         fmt.Println(err.Error())
     }
+}
 
+func (p *Process) Lookup() error {
+    db, err := bolt.Open("processes.db", 0600, nil)
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+    err = db.View(func(tx *bolt.Tx) error {
+        b := tx.Bucket([]byte("processes"))
+        v := b.Get(p.Key())
+        if v == nil {
+            return fmt.Errorf("%s does not know this process. Run the command in capture mode first.", os.Args[0])
+        }
+        p.FromJson(v)
+        return nil
+    })
+    return err
+}
+
+func (p *Process) Playback() error {
+    os.Stdout.Write(p.Stdout)
+    os.Stderr.Write(p.Stderr)
+    os.Exit(p.ReturnCode)
+    return nil
+}
+
+func main() {
+    p := Process{}
+    p.Command = os.Args[1:]
+    p.Capture()
+    if err := p.Lookup() ; err == nil {
+        p.Playback()
+    } else {
+        fmt.Println(err.Error())
+    }
 }
