d2f24464a39480d2a649d2ac170d7e512b463793
Bart Spaans <bart.spaans@opencredo.com>
2016-05-20 16:41:41 +0100

    new python-myna release

---
+++ contrib/python-myna/myna/__init__.py
@@ -0,0 +1,11 @@
+import shim
+
+tmpdir = None
+
+def setUp():
+    global tmpdir
+    tmpdir = shim.setup_shim_for('kubectl')
+
+def tearDown():
+    global tmpdir
+    shim.teardown_shim_dir(tmpdir)
--- contrib/python-myna/setup.py
+++ contrib/python-myna/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "python-myna",
-    version = "1.0.2",
+    version = "1.1.0",
     packages = find_packages(),
     author = "SpectoLabs",
     author_email = "admin@specto.io",
